ccd34c51e8677831d5c0e973a63f0e406c2de6ab
font <francesco.fontana4@studenti.unimi.it>
2023-08-06 22:53:25 +0200

    TRIGGER no_iscriz_se_non_in_CdL con funione check_appartenenza_cdl() in triggers.sql

--- SQL/triggers.sql
+++ SQL/triggers.sql
@@ -1,3 +1,5 @@
+------------------------------------------------------------------------------
+------ VERIFICA CHE IN UNA DATA d NON ESISTANO DUE ESAMI DELLO STESSO ANNO
 CREATE OR REPLACE FUNCTION check_inserimento_esame() RETURNS TRIGGER as $$
 BEGIN
     PERFORM * FROM calendario_esami c
@@ -18,9 +20,33 @@ CREATE OR REPLACE TRIGGER no_esami_stesso_anno_stesso_giorno
 BEFORE INSERT OR UPDATE ON calendario_esami
 FOR EACH ROW
 EXECUTE FUNCTION check_inserimento_esame();
+------------------------------------------------------------------------------
+------ VERIFICA CHE L'INSEGNAMENTO A CUI LO STUDENTE SI VUOLE ISCRIVERE FACCIA PARTE DEL SUO CdL
+CREATE OR REPLACE FUNCTION check_appartenenza_cdl() RETURNS TRIGGER as $$
+BEGIN
+    PERFORM * FROM calendario_esami c
+            INNER JOIN iscrizione i ON c.id = i.esame
+            INNER JOIN insegnamento_parte_di_cdl ipc ON ipc.insegnamento = c.insegnamento
+            INNER JOIN studente s ON s.corso_di_laurea = ipc.corso_di_laurea
+            WHERE s.utente = NEW.studente;
+    IF FOUND THEN
+        -- l'esame che si vuole inserire in calendario_esami è presente nel cdL
+        RETURN NEW;
+    ELSE
+        -- l'esame che si vuole inserire in calendario_esami NON è presente nel cdL
+        RAISE NOTICE 'ATTENZIONE: l''esame a cui ti vuoi iscrivere non fa parte del CdL a cui è iscritto lo studente';
+        PERFORM pg_notify('notifica', 'ATTENZIONE: l''esame a cui ti vuoi iscrivere non fa parte del CdL a cui è iscritto lo studente');
+        RETURN NULL;
+    END IF;
+END;
+$$ language 'plpgsql';
 
-
-
+CREATE OR REPLACE TRIGGER no_iscriz_se_non_in_CdL
+    BEFORE INSERT OR UPDATE ON iscrizione
+    FOR EACH ROW
+EXECUTE FUNCTION check_appartenenza_cdl();
+-------------------------------------------------------------------------------
+------ VERIFICA CHE VENGA RISPETTATA LA PROPEDEUTICITA TRA I CORSI PER ISCRIVERSI A UN ESAME
 CREATE OR REPLACE FUNCTION check_propedeuticita() RETURNS TRIGGER as $$
 BEGIN
     -- cerco se c'è in propedeuticità e nel caso che in carriera 'insegnamento1' sia superato
@@ -43,10 +69,11 @@ CREATE OR REPLACE TRIGGER no_esami_senza_propedeuticita
     FOR EACH ROW
 EXECUTE FUNCTION check_propedeuticita();
 
-
+-------------------------------------------------------------------------------
+------ INSERISCI RIGHE NELLA TABELLA CARRIERA APPENA INSERITO UN NUOVO STUDENTE
 CREATE OR REPLACE FUNCTION inserisci_in_carriera() RETURNS TRIGGER as $$
 BEGIN
-    -- cerco se c'è in propedeuticità e nel caso che in carriera 'insegnamento1' sia superato
+    -- creo la carriera (con tutti i voti del CdL pari a 0) lo studente appena inserito
     INSERT INTO carriera (studente, insegnamento, valutazione, data)
     SELECT NEW.utente, i.insegnamento, 0, NULL
     FROM insegnamento_parte_di_cdl i
@@ -56,7 +83,8 @@ END;
 $$ language 'plpgsql';
 
 CREATE OR REPLACE TRIGGER creazione_carriera
-    AFTER INSERT OR UPDATE ON studente
+    AFTER INSERT ON studente
     FOR EACH ROW
 EXECUTE FUNCTION inserisci_in_carriera();
 
+-------------------------------------------------------------------------------
