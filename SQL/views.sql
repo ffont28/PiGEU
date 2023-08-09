a7fa979ae1222885871d80743440b4a237acf29c
font <francesco.fontana4@studenti.unimi.it>
2023-08-09 02:59:29 +0200

    Assegnazione prossime attività da fare nella parte SQL trigger

--- SQL/views.sql
+++ SQL/views.sql
@@ -1,6 +1,6 @@
 ------------------------------------------------ PAGINA DI CREAZIONE VISTE --------------------------------------------
 -- CREO UNA VISTA PER PRODURRE LE INFORMAZIONI DI UN CORSO DI LAUREA - specifica 2.2.6
------ scelgo di fare una vista non materializzata perchè così da non essere persistente in memoria
+----- scelgo di fare una vista non materializzata così da non essere persistente in memoria
 CREATE or replace VIEW informazioni_CdL AS
 SELECT c.codice, c.nome, c.tipo, i.codice codicec, i.nome nomec, ip.anno , i.descrizione, i.cfu, u.nome nomedoc, u.cognome cognomedoc
 FROM corso_di_laurea c INNER JOIN insegnamento_parte_di_cdl ip ON c.codice = ip.corso_di_laurea
