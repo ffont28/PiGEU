2894ac386f29806321ba22def7bec3d0ad3b9b82
font <francesco.fontana4@studenti.unimi.it>
2023-08-14 08:39:35 +0200

    CODE REVIEW: bug fixed errori di javascript

--- functions.php
+++ functions.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
 include('conf.php');
-?>
-<script></script>
-<?php
+
 function importVari(){
     echo '  <!-- import di Bootstrap-->
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
